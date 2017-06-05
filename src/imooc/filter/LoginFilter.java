0e857bfba8c54a11d15105caeeee3c3192e69123
干干 <yg@ygdeMacBook-Pro.local>
2017-06-05 22:54:26 +0800

    java web 重新导入

---
+++ src/imooc/filter/LoginFilter.java
@@ -0,0 +1,63 @@
+package imooc.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class LoginFilter implements Filter {
+
+	private FilterConfig config;
+	
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2) throws IOException, ServletException {
+
+		HttpServletRequest request = (HttpServletRequest) arg0;
+		HttpServletResponse response = (HttpServletResponse) arg1;
+		HttpSession session = request.getSession();
+		
+		String noLoginPaths = config.getInitParameter("noLoginPaths");
+		
+		String charset = config.getInitParameter("charset");
+		if(charset==null){
+			charset = "UTF-8";
+		}
+		request.setCharacterEncoding(charset);
+		
+		if(noLoginPaths!=null){
+			String[] strArray = noLoginPaths.split(";");
+			for (int i = 0; i < strArray.length; i++) {
+				
+				if(strArray[i]==null || "".equals(strArray[i]))continue;
+				
+				if(request.getRequestURI().indexOf(strArray[i])!=-1 ){
+					arg2.doFilter(arg0, arg1);
+					return;
+				}
+			}
+			
+		}
+		
+		
+		
+		
+		if(session.getAttribute("username")!=null){
+			arg2.doFilter(arg0, arg1);
+		}else{
+			response.sendRedirect("login.jsp");
+		}
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		config = arg0;
+	}
+
+}
---
+++ src/imooc/serlvet/LoginServlet.java
@@ -0,0 +1,61 @@
+package imooc.serlvet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public LoginServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 * This method is called when a form has its tag value method equals to post.
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		System.out.println(username);
+		
+		if("admin".equals(username) && "admin".equals(password)){
+
+			HttpSession session = request.getSession();
+			session.setAttribute("username", username);
+			response.sendRedirect(request.getContextPath()+"/sucess.jsp");
+		} else {
+			response.sendRedirect(request.getContextPath()+"/fail.jsp");
+		}
+	}
+
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
